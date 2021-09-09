1ca566c74a5d46694b248167d37a568bb6234dbf
Delmerk <delmerk2000@gmail.com>
2021-09-09 10:23:46 -0500

    09-9-2021 - 10:23am

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
+
         // return User::paginate(10);
         return User::orderBy('id', 'desc')->get();
     }
